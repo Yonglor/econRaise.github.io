3df76e6b12ef403f07ffc7fdd9ddfb9619dd54b3
Yonglor <159979410+Yonglor@users.noreply.github.com>
2024-02-14 13:01:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ alert("치킨을 획득 했다!")
 function wrok() {
     alert("돈부터 벌어오자!")
 }
+
+function ener() {
+    money -= 2000
+    displayElement.innerHTML = money;
+    increaseMoney = function() {
+        money += 2;
+        displayElement.innerHTML = money;
+    }
+       
+}
