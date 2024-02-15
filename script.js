3abbbb2e7b4aa2c8c227736b254c61f2bf20b8d2
Yonglor <159979410+Yonglor@users.noreply.github.com>
2024-02-15 22:03:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+let ending = 'https://www.youtube.com/watch?v=4ouHTwdvQCs'
 function chi() {
 money -= 12000
 displayElement.innerHTML = money;
 alert("치킨을 획득 했다!")
+location.href=ending
 }
 
 function wrok() {
