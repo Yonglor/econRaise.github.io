ca7ffc6f8f6cda140aac018bbefd668f707b9a0d
Yonglor <159979410+Yonglor@users.noreply.github.com>
2024-02-16 11:42:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function chi() {
 money -= 12000
 displayElement.innerHTML = money;
 alert("치킨을 획득 했다!")
-location.href = ending;
+location.href=ending
 }
 
 function wrok() {
@@ -11,11 +11,22 @@ function wrok() {
 }
 
 function ener() {
-    money -= 2000
+    money -= 1600
     displayElement.innerHTML = money;
     increaseMoney = function() {
         money += 2;
         displayElement.innerHTML = money;
     }
        
+}
+let colaimg = 'https://img.danawa.com/prod_img/500000/012/172/img/3172012_1.jpg?_v=20220801091506'
+function cole() {
+    money -= 1900
+    displayElement.innerHTML = money;
+    alert("축하합니다! 돈을 날리셨습니다! 그래도 괜찮아요, 콜라는 맛있으니까요!")
+    location.href=colaimg
+}
+
+function test() {
+    alert("돈부터 벌어옵시다!")
 }
